377635bf4be178c81749cdde6f96c4f96f2ff829
fog <1134921599@qq.com>
2020-08-13 18:56:32 +0800

    第三章的课后第五题至第六题

---
+++ ProgrammingInC/chapter03/practice/practice05.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int value1 = 25;
+    int value2 = 37;
+    int value3 = 19;
+
+    int sum = value1 + value2 - value3;
+
+    /*Compute result*/
+    sum = 25 + 37 - 19;
+
+    /*Display result*/
+    printf("The answer is %i.\n", sum);
+
+    return 0;
+
+}
---
+++ ProgrammingInC/chapter03/practice/practice06.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int answer,result;
+
+    answer = 1000;
+    result = answer - 10;
+    printf ("The result is %i\n", result + 5);
+
+    return 0;
+}
