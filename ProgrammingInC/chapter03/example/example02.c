68897fca7d5aeaccc15b340e95d6a7038d213874
fog <1134921599@qq.com>
2020-08-18 14:21:07 +0800

    针对每个文件的末端增加了一行空格

--- ProgrammingInC/chapter03/example/example02.c
+++ ProgrammingInC/chapter03/example/example02.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    printf("Programming is fun.\n");
-    printf("And Programming in C is even more fun.\n");
+    printf ("Programming is fun.\n");
+    printf ("And Programming in C is even more fun.\n");
 
     return 0;
 }
