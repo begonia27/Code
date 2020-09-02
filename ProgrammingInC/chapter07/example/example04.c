17737625b2f1d56f2298acf58bf9d07110af5d56
fog <1134921599@qq.com>
2020-09-02 20:57:39 +0800

    第七章第四题（添加备注）

--- ProgrammingInC/chapter07/example/example04.c
+++ ProgrammingInC/chapter07/example/example04.c
@@ -15,25 +15,32 @@ int main (void)
     //数字可以容纳50个元素，p从5开始，每次增加2,展示所有的奇数
     for (p = 5; p <= 50; p = p + 2)
     {   
-        //当p是小于等于50的数，则执行本程序
+        //当p是质数，则执行本程序
         isPrime = true;
         
-        //判断primes[i]数组的值是否超过p的平方根
+        //isPrime成立并且p大于或等于primes[i]的平方根
         for (i = 1; isPrime && p / primes[i] >= primes[i]; ++i)
-        {
+        {   
+            //当上面的条件成立时，执行这个判断语句
             if (p % primes[i] == 0)
-            {
+            {   
+                //经判断，如果isPrime不是质数，继续外层的for循环，直到不满足循环条件
                 isPrime = false;
             }
         }
-
+        
+        //如果isPrime是质数
         if (isPrime == true)
-        {
+        {   
+            //则将primeIndex和p的值分别进行赋值
             primes[primeIndex] = p;
+
+            //继续进行primeindex的循环
             ++primeIndex;
         }
     }
 
+    //循环将每个数组的值都打印出来（为什么i < primeIndex？因为数组的元素要小于个数）
     for (i = 0; i < primeIndex; ++i)
     {
         printf("%i ", primes[i]);
