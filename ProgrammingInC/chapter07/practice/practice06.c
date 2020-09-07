dcc5d142011987fae28228661875df6692a9a102
fog <1134921599@qq.com>
2020-09-07 19:08:35 +0800

    第七章的课后习题的第六题

---
+++ ProgrammingInC/chapter07/practice/practice06.c
@@ -0,0 +1,25 @@
+// Program to gennerate the first 15 Fibonacci numbers
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0, n = 1, count;
+
+    printf("%i\n%i\n", i, n);
+
+    i = i + 1;
+    n = n + 1; 
+
+    for (i = 0, n = 1; count < 6; ++count)
+    {
+        i += n;
+        n += i;
+
+        printf("%i\n%i\n", i, n);       
+    }
+
+    i += n;
+
+    printf("%i\n", i);
+    return 0;
+}
