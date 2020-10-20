8bbab8291b514093567adb07b6f03dc9cb009a41
fog <1134921599@qq.com>
2020-10-20 18:32:19 +0800

    第八章的课后习题的第十题（使用全局变量）

--- ProgrammingInC/chapter08/practice/practice10_test2.c
+++ ProgrammingInC/chapter08/practice/practice10_test2.c
@@ -3,51 +3,56 @@
 #include <stdbool.h>
 #include <math.h>
 
-int number, a, n;
-bool isPrime = true;
-
-void getNumber(void)
+int getNumber(void)
 {
+    int number;
     printf("Please enter a number to calculate and judgment prime: ");
     scanf("%i", &number);
-    a = number;
+   
+    return number;
 }
 
-void judgumentNumber(void)
+int judgumentNumber(int number, int n)
 {
+    bool isPrime = true;
+
     if (number < 2)
     {
         isPrime = false;
-        
-        // return代表不返回值，因为返回值类型要写成void类型
-        return;
+        return false;
     }
 
     if (number == 2)
     {
         isPrime = true;
-        return;
+        return true;
     }
 
     if (number % 2 == 0)
     {
         isPrime = false;
-        return;
+        return false;
     }
 
     for (n = 3; n < sqrt(number); n += 2)
     {
         if (number % n == 0)
         {
-            isPrime = false;
             // 代表不是质数
+            isPrime = false;
+
             break;
         }
     }
 }
 
 void printNumber(void)
-{
+{   
+    int a, number;
+    bool isPrime;
+
+    a = getNumber();
+
     if (isPrime == true)
     {
         /* 此处的a是原始的输入值number,只不过judgumentNumber()函数里面使用了number,
@@ -62,9 +67,14 @@ void printNumber(void)
 }
 
 int main(void)
-{
+{   
+    int number, n, a;
+    bool isPrime;
+
+    a = number;
+    
     getNumber();
-    judgumentNumber();
+    judgumentNumber(number, n);
     printNumber();
 
     return 0;
---
+++ ProgrammingInC/chapter08/practice/practice11.c
@@ -0,0 +1,37 @@
+// 接收一个数组及其长度为参数，返回值是参数数组中所有元素的和
+
+#include <stdio.h>
+
+/*int arraySum(int arraySum[3])
+{   
+    int i, result;
+    int arraySum[0] = {2};
+    int arraySum[1] = {3};
+    int arraySum[2] = {4};
+
+    for (i = 0; i < 3; ++i)
+    {
+        result += arraySum[i];
+    }
+
+    return result;
+}*/
+
+int main(void)
+{   
+    int arraySum[i];
+
+    int i, result;
+    int arraySum[0] = {2};
+    int arraySum[1] = {3};
+    int arraySum[2] = {4};
+
+    for (i = 0; i < 3; ++i)
+    {
+        result += arraySum[i];
+    }
+
+    printf("%i\n");
+
+    return 0;
+}
