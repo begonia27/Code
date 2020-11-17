f8443a6cd9b14d44301e5516815f85f099febf62
fog <1134921599@qq.com>
2020-11-17 18:03:01 +0800

    复合字面量程序示例

--- ProgrammingInC/test/functionTest.c
+++ ProgrammingInC/test/functionTest.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 void func1(void);
 void func2(int n, int m);
@@ -17,11 +17,13 @@ int main(void)
     func5(p, q);
 }
 
+// 函数无返回值并且没有参数，函数体内打印一句话
 void func1(void)
 {
     printf("no parameter no return\n");
 }
 
+// 函数无返回值并且传进来两个参数，函数体内对两个参数求和
 void func2(int n, int m)
 {
     int sum = n + m;
@@ -29,6 +31,7 @@ void func2(int n, int m)
     printf("the sum is %i\n", sum);
 }
 
+// 函数无返回值并且无参数，函数体内对两个变量进行求和
 void func3(void)
 {
     int n = 4;
@@ -38,11 +41,14 @@ void func3(void)
     printf("the sum is %i\n", sum);
 }
 
+// 函数无返回值并且传进来两个参数，这两个参数调用第二个函数
 void func4(int p, int q)
 {
     func2(p , q);
 }
 
+// 函数无返回值并且有两个参数，这两个参数乘以2，并将结果返回给第二个函数
+// 函数无返回值并且有两个参数，这两个参数调用第二个函数
 void func5(int p, int q)
 {
     func2(p * 2, q * 2);
