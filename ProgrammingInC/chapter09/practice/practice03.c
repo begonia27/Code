345bfd0326e23f32a6e3b676256559d2b3530c08
fog <1134921599@qq.com>
2020-11-30 17:35:29 +0800

    第九章的课后习题的第三题

--- ProgrammingInC/chapter09/practice/practice03.c
+++ ProgrammingInC/chapter09/practice/practice03.c
@@ -1,6 +1,8 @@
 // 计算两个时间之间相差多长时间
 #include <stdio.h>
 
+struct time elapsed_time(struct time time1, struct time time2);
+
 struct time
 {
     int hour;
@@ -10,25 +12,24 @@ struct time
 
 int main(void)
 {
-    struct time elapsed_time(struct time time1, struct time time2,
-        struct time time3);
-    
-    struct time elapsed_time(time1, time2, time3);
-
-    printf("The elapsed time is %i \n",
-        time3.hour, time3.minutes, time3.seconds);
-}
-
-struct time elapsed_time(struct time time1, struct time time2, struct time time3)
-{   
     struct time time1 = {0,0,0};
     struct time time2 = {0,0,0};
     struct time time3 = {0,0,0};
 
-    printf("Please enter the time (hh:mm:ss): ");
+    struct time result = elapsed_time(time1, time2);
+
+    printf("The elapsed time is %i hour %i minutes %i seconds\n",
+        result.hour, result.minutes, result.seconds);
+}
+
+struct time elapsed_time(struct time time1, struct time time2)
+{   
+    struct time time3;
+
+    printf("Please enter the first time (hh:mm:ss): ");
     scanf("%i:%i:%i", &time1.hour, &time1.minutes, &time1.seconds);
 
-    printf("Plsase enter the time (hh:mm:ss): ");
+    printf("Plsase enter the second time (hh:mm:ss): ");
     scanf("%i:%i:%i", &time2.hour, &time2.minutes, &time2.seconds); 
 
     if (time1.hour || time2.hour != 24)
@@ -37,11 +38,34 @@ struct time elapsed_time(struct time time1, struct time time2, struct time time3
         time3.minutes = time2.minutes - time1.minutes;
         time3.seconds = time2.seconds - time1.seconds;
 
+        if (time3.hour < 0)
+        {   
+            // hour is negative number
+            time3.hour = time2.hour - time1.hour + 24;
+            time3.minutes = time2.minutes - time1.minutes;
+            time3.seconds = time2.seconds - time1.seconds;
+
+            if (time3.minutes < 0)
+            {   
+                // hour is negative number
+                time3.hour = time2.hour - time1.hour + 23;
+                time3.minutes = time2.minutes - time1.minutes + 60;
+                time3.seconds = time2.seconds - time1.seconds;
+
+                if (time3.seconds < 0)
+                {   
+                    // hour is negative number
+                    time3.hour = time2.hour - time1.hour + 23;
+                    time3.minutes = time2.minutes - time1.minutes + 59;
+                    time3.seconds = time2.seconds - time1.seconds + 60;
+                }
+            }
+        }
+
         return time3;
     }
     else
-    {   
-        // midnight
+    {
         time3.hour = time2.hour - time1.hour + 24;
         time3.minutes = time2.minutes - time1.minutes;
         time3.seconds = time2.seconds - time1.seconds;
