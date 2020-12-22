b020e04dc2c6477506a801dcd43ad7cffb855ba1
fog <1134921599@qq.com>
2020-12-22 18:10:42 +0800

    第十章的第五个示例

---
+++ ProgrammingInC/chapter10/example/example05.c
@@ -0,0 +1,15 @@
+// 使用scanf函数读取字符串的函数
+#include <stdio.h>
+
+int main(void)
+{
+    char s1[81], s2[81], s3[81];
+
+    printf("Enter text:\n");
+
+    scanf("%s80%s80%s80", s1, s2, s3);
+
+    printf("\ns1 = %s\ns2 = %s\ns3 = %s\n", s1, s2, s3);
+
+    return 0;
+}
