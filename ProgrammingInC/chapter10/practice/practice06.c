ddc01144592dbc16ba087eb9b2a4ffde5db3ffa8
fog <1134921599@qq.com>
2021-03-22 20:07:37 +0800

    第十章的课后习题的第六题

--- ProgrammingInC/chapter10/practice/practice06.c
+++ ProgrammingInC/chapter10/practice/practice06.c
@@ -1,44 +1,56 @@
 #include <stdio.h>
 
-char removeString(const char text[], int indexOfStart, int countOfDelete, char answer)
+char removeString(char text[], int indexOfStart, int countOfDelete, char answer)
 {
-    int countOfDelete = 6, countOfAllString;
+    int countOfAllString;
     char result;
-    int i, j = 0;
+    int i, j, k = 0;
 
     // 输出原字符串的整体
     for (countOfAllString = 0; text[countOfAllString] != '\0'; ++countOfAllString)
     {
-        printf("%c\0.\n", text[countOfAllString]);
+        printf("%c", text[countOfAllString]);
     }
+
+    printf("\n");
 
     // 根据循环和判断，输出不要的字符串
-    for (int i = 4; text[i] != '\0'; ++i)
+    for (int i = indexOfStart; text[i] != '\0' && (i < indexOfStart + countOfDelete); ++i)
     {
-        if (i < (indexOfStart + countOfDelete))
-        {
-            printf("%c\0.\n", text[i]);
-        }
+        printf("%c", text[i]);
     }
 
+    printf("\n");
+
     // 在长字符串中去掉不要的字符串，剩余的字符串就是我们要留下来的字符串
-    for (j = 0; text[countOfAllString] != text[i]; ++j)
+    for (j = 0; text[countOfAllString] != '\0'; ++j)
     {
-        printf("%c\0.\n", text[j]);
+        for (i = 0; text[i] != '\0'; ++i)
+        {
+            if (text[countOfAllString] == text[i])
+            {
+                printf("same.\n");
+            }
+            else
+            {
+                printf("don't same.\n");
+            }
+        }
     }
 }
 
 int main(void)
 {
-    char removeString(const char text[], int indexOfStart, int countOfDelete, char answer);
+    char removeString(char text[], int indexOfStart, int countOfDelete, char answer);
 
-    const char text[] = "the wrong son";
-    int countOfDelete = 0, countOfAllString = 0;
+    char text[] = "the wrong son";
+    int countOfDelete;
+    int countOfAllString = 0;
     char result;
-    int i, j = 0;
+    int i, j, k = 0;
 
     // 判断要删减的字符串的字符个数和原字符串中字符个数的长度关系
-    if (countOfDelete < countOfAllString)
+    if (countOfDelete <= countOfAllString)
     {
         removeString("the wrong son", 4, 6, result);
     }
